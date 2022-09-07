2129d388045cfbc897ed966403459962d1fe2030
adieflores13 <83394155+adieflores13@users.noreply.github.com>
2022-09-07 20:00:39 +0800

    update login
    update login

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -13,26 +13,53 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.pink[200],
+        backgroundColor: Colors.grey[300],
         body: Center(
           child: SafeArea(
-            child: Column(children: [
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
               Text(
                 'Welcome!',
                 style: TextStyle(
-                  color: Colors.white,
+                  color: Colors.black,
                   fontWeight: FontWeight.bold,
-                  fontSize: 24,
+                  fontSize: 40,
                 ),
               ),
-              SizedBox(height: 10),
+              SizedBox(height: 20),
               Text(
-                'Please Login Again. We missed you!',
+                'Please login again. We missed you!',
                 style: TextStyle(
-                  color: Colors.white,
+                  color: Colors.black,
                   fontSize: 14,
                 ),
               ),
+              SizedBox(height: 20),
+
+              //EMAIL//
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(13),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.person),
+                        prefixIconColor: Colors.black,
+                        border: InputBorder.none,
+                        hintText: 'Email',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              //PASSWORD//
               SizedBox(height: 10),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
@@ -42,15 +69,54 @@ class _LoginPageState extends State<LoginPage> {
                     border: Border.all(color: Colors.white),
                     borderRadius: BorderRadius.circular(13),
                   ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.person),
-                      prefixIconColor: Colors.black,
-                      border: InputBorder.none,
-                      hintText: 'Email',
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.lock),
+                        prefixIconColor: Colors.black,
+                        border: InputBorder.none,
+                        hintText: 'Password',
+                      ),
                     ),
                   ),
                 ),
+              ),
+
+              //LOGIN//
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.blue[400],
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+
+              //NOT A MEMBER?//
+
+              Text(
+                ' Not a member?',
+                style: TextStyle(),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 7),
+              Text(
+                ' Register Now',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+                textAlign: TextAlign.center,
               ),
             ]),
           ),
