65bff4b7c865ccefe063084b4f80285968d6ff81
adieflores13 <83394155+adieflores13@users.noreply.github.com>
2022-09-08 20:32:51 +0800

    UI Spaces Login Button

--- lib/signup.dart
+++ lib/signup.dart
@@ -14,142 +14,149 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: Center(
-        child: SafeArea(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text('Welcome to our App!',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 40,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Please fill out all the forms below',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-              ),
-            ),
-            SizedBox(height: 20),
-
-            //FULL NAME//
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(13),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.person),
-                      prefixIconColor: Colors.black,
-                      border: InputBorder.none,
-                      hintText: 'Full Name',
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            //PHONE NUMBER//
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(13),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.person),
-                      prefixIconColor: Colors.black,
-                      border: InputBorder.none,
-                      hintText: 'Phone Number',
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            //EMAIL//
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(13),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.person),
-                      prefixIconColor: Colors.black,
-                      border: InputBorder.none,
-                      hintText: 'Email',
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            //PASSWORD//
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(13),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.lock),
-                      prefixIconColor: Colors.black,
-                      border: InputBorder.none,
-                      hintText: 'Password',
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            //LOGIN//
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.blue[400],
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: Center(
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
-          ]),
+        body: Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF701ebd),
+            Color(0xFF873bbc),
+            Color(0xFFfe4a97),
+            Color(0xFFe17763),
+            Color(0xFF68998c),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
         ),
-      )
-    );
+      ),
+      child: SafeArea(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            'Welcome to our App!',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w400,
+              fontSize: 35,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Please fill out all the forms below',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+            ),
+          ),
+          SizedBox(height: 20),
+
+          //FULL NAME//
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(13),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.person),
+                    prefixIconColor: Colors.black,
+                    border: InputBorder.none,
+                    hintText: 'Full Name',
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          //PHONE NUMBER//
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(13),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.person),
+                    prefixIconColor: Colors.black,
+                    border: InputBorder.none,
+                    hintText: 'Phone Number',
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          //EMAIL//
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(13),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.person),
+                    prefixIconColor: Colors.black,
+                    border: InputBorder.none,
+                    hintText: 'Email',
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          //PASSWORD//
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(13),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.lock),
+                    prefixIconColor: Colors.black,
+                    border: InputBorder.none,
+                    hintText: 'Password',
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          //LOGIN//
+          SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Sign Up'),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    ));
   }
 }
